e079d811e494eb140ed786304f3aff18ee318b11
BinalPatel29 <binaldpatel29@gmail.com>
2026-06-26 12:14:37 +0530

    replace get routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,25 +1,27 @@
 import express from 'express';
-import { readNotes, saveNotes } from '../storage.js';
 import { Router } from 'express';
 import Note from '../models/Note.js';
+const router = Router();
 
-Router.get('/' , async(req,res,next) => {
-    try{
+router.get('/' , async (req, res, next) => {
+    try {
         const notes = await Note.find();
         res.json(notes);
     }
-    catch(error){
+    catch (error) {
         next(error);
     }
 });
 
-Router.get('/:id', async(req,res,next) => {
-    try{
-        const note = await Note.findById(req,URLSearchParams.id);
-        if(!note) return res.status(404).json({ error: 'Note not found' });
+router.get('/:id', async (req, res, next) => {
+    try {
+        const note = await Note.findById(req.params.id);
+        if (!note) return res.status(404).json({ error: 'Note not found' });
         res.json(note);
     }
-    catch(error){
+    catch (error) {
         next(error);
     }
 });
+
+export default router;
