15fe37e907efb64a3fbc54535ef3e9f994fbf766
BinalPatel29 <binaldpatel29@gmail.com>
2026-06-26 12:13:46 +0530

    create models/Note.js file

--- models/Note.js
+++ models/Note.js
@@ -2,17 +2,18 @@ import mongoose from "mongoose";
 
 const notesSchema = new mongoose.Schema({
     text: {
-        type : String,
-        required : true,
-        trim : true
+        type: String,
+        required: true,
+        trim: true
     },
-    createdAt : {
-        type : String,
-        default : () => {
+    createdAt: {
+        type: String,
+        default: () => {
             const now = new Date();
-            return `${String(now.getDate()).padStart(2,'0')}/ ${String(now.getMonth()+1).padStart(2,'0')}/ ${now.getFullYear()}`;
+            return `${String(now.getDate()).padStart(2, '0')}/${String(now.getMonth() + 1).padStart(2, '0')}/${now.getFullYear()}`;
         }
     }
 });
+
 const Note = mongoose.model('Note', notesSchema);
 export default Note;
