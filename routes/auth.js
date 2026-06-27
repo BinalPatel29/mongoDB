48cc0830398ddb0ade81124f5bc02d4f79f3b426
BinalPatel29 <binaldpatel29@gmail.com>
2026-06-27 16:50:31 +0530

    create JWT

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+
 const router = express.Router();
 
 router.post('/register', async (req, res, next) => {
@@ -20,4 +22,31 @@ router.post('/register', async (req, res, next) => {
         next(error);
     }
 });
+
+router.post('/login', async(req,res,next) => {
+    try{
+        const { email, password } = req.body;
+        if(!email || !password){
+            return res.status(400).json({ error : 'Email and password required' });
+        }
+        const user = await User.findOne({ email });
+        if(!user){
+            return res.status(401).json({ error : 'Invalid credentials' });
+        }
+        const isMatch = await user.comparePassword(password);
+        if(!isMatch){
+            return res.status(401).json({ error : 'Invalid credentials' });
+        }
+        const token = jwt.sign (
+            { userId: user._id } ,
+            process.env.JWT_SECRET,
+            { expiresIn: '24h' }
+        );
+        res.json({ token });
+    } 
+    catch(error){
+        next(error);
+    }
+});
+
 export default router;
