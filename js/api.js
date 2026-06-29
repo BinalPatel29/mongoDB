9bb32e1da30a3543f62fab5ca1a73c0c759d26ae
BinalPatel29 <binaldpatel29@gmail.com>
2026-06-29 11:47:36 +0530

    fetch api

--- js/api.js
+++ js/api.js
@@ -1,14 +1,14 @@
 async function apiFetch(endpoint,options){
     const baseUrl = "http://localhost:3000/api";
     const token = localStorage.getItem('token');
-    const header = {
+    const headers = {
             "Content-Type" : "application/json",
             "Authorization" : `Bearer ${token}` 
         }
     const url = endpoint.startsWith('http') ? endpoint : `${baseUrl}${endpoint}`;
 
     try{
-        const response = await fetch(url,Headers,options);
+        const response = await fetch(url,headers,options);
         if(!response.ok){
             throw new Error('Response status: ${response.status}');
         }
